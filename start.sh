b89342ffe55854233e5254d07dd162bd0aeb6cd4
honwen.chan <honwen.chan@gmail.com>
2016-12-02 13:28:21 +0800

    v20161201

--- start.sh
+++ start.sh
@@ -6,4 +6,5 @@ KCP_PASS=${KCP_PASS:-kcptunPassword}
 sed "s/shadowsocksPassword/${SSR_PASS}/g" /etc/shadowsocks.json > /etc/shadowsocks_mod.json
 sed "s/kcptunPassword/${KCP_PASS}/g" /etc/kcptun.json > /etc/kcptun_mod.json
 
-/usr/bin/supervisord
+/usr/bin/supervisord -c /etc/supervisord.conf
+
